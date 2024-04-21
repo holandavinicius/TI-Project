7406c450f932c56dc57f1db7feabdaee59bb9d55
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2024-04-21 01:28:18 +0100

    refact: fixing device_data_model about fields with wrong datatype.

--- api/device_data_model.php
+++ api/device_data_model.php
@@ -3,38 +3,41 @@
 
 class DeviceDataModel {
 
-    private string $_time;
-    private array $_log;
-    private string $_name;
-    private float $_value;
+    private $_time;
+    private $_log;
+    private $_name;
+    private $_value;
 
 
     public function __construct($_name,$_time,$_value) {
         
         $this->_time = $_time;
-        $this->_log = array();
+        $this->_log = "";
         $this->_name = $_name;
         $this->_value = $_value;
     }
    
-    public function getTime() : string{
+
+    public function getTime(){
         return $this->_time;
     }
 
-   
-    public function getLog() : array{
+    public function setLog($log){
+
+        $this->_log = $log;
+    }
+
+
+    public function getLog(){
         return $this->_log;
     }
 
-    public function setLog($_log){
-        $this->_log = $_log;
-    }
-    public function getName() : string{
+    public function getName(){
         return $this->_name;
     }
 
     
-    public function getValue() : string{
+    public function getValue(){
         return $this->_value;
     }
 
