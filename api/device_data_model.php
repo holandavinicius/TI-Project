3c920231a39121b671157b3f6e6fd2b989dea505
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2024-04-21 21:47:56 +0100

    bug: bug fix for fields in model device class.

--- api/device_data_model.php
+++ api/device_data_model.php
@@ -3,44 +3,43 @@
 
 class DeviceDataModel {
 
-    private $_time;
-    private $_log;
-    private $_name;
-    private $_value;
+    private $time;
+    private $log;
+    private $name;
+    private $value;
 
 
-    public function __construct($_name,$_time,$_value) {
+    public function __construct($name,$time,$value) {
         
-        $this->_time = $_time;
-        $this->_log = "";
-        $this->_name = $_name;
-        $this->_value = $_value;
+        $this->time = $time;
+        $this->log = "";
+        $this->name = $name;
+        $this->value = $value;
     }
    
 
-    public function getTime(){
-        return $this->_time;
-    }
+  
 
     public function setLog($log){
-
         $this->_log = $log;
     }
 
 
     public function getLog(){
-        return $this->_log;
+        return $this->log;
     }
 
     public function getName(){
-        return $this->_name;
+        return $this->name;
     }
 
     
     public function getValue(){
-        return $this->_value;
+        return $this->value;
     }
 
-
+    public function getTime(){
+        return $this->time;
+    }
 
 }
