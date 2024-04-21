f99dc8b37015d91b2c254947b98204724ece9403
dhiegoferreira <dev.dhiegoferreira@gmail.com>
2024-04-21 21:49:22 +0100

    bug: fixing get values from file and convert lowercase to each device received by api. Log now is working.

--- api/device_data_service.php
+++ api/device_data_service.php
@@ -1,23 +1,27 @@
 <?php
 
 
-require_once("../TI-Project/api/device_data_interface.php");
-require_once("../TI-Project/api/device_data_model.php");
+require_once("C:/UniServerZ/www/ti/api/device_data_interface.php");
+require_once("C:/UniServerZ/www/ti/api/device_data_model.php");
 
 
 class DeviceDataService implements DeviceDataInterface {
     
-    CONST RelativePath = "../TI-Project/api/files/";
+    CONST RelativePath = "../ti/api/files/";
     public function ProcessDataPost(DeviceDataModel $_deviceData){
 
         
         $path =  self::RelativePath.$_deviceData->getName();
         
-        file_put_contents($path."/nome.txt", $_deviceData->getName() . PHP_EOL, FILE_TEXT);
-        file_put_contents($path."/valor.txt", $_deviceData->getValue() . PHP_EOL, FILE_APPEND);
-        file_put_contents($path."/hora.txt", $_deviceData->getTime() . PHP_EOL, FILE_APPEND);
+        $path = strtolower($path);
 
-        $log = "nome: ".$_deviceData->getName()." hora:".$_deviceData->getTime()." valor:".$_deviceData->getValue()." ".PHP_EOL;
+        print_r($path);
+
+        file_put_contents($path."/nome.txt",$_deviceData->getName(), FILE_TEXT);
+        file_put_contents($path."/valor.txt",$_deviceData->getValue(), FILE_TEXT);
+        file_put_contents($path."/hora.txt",$_deviceData->getTime(), FILE_TEXT);
+
+        $log = "nome: ".$_deviceData->getName()." hora:".date("Y-m-d").date("h:i:sa")." valor:".$_deviceData->getValue();
         file_put_contents($path."/log.txt",   $log.PHP_EOL, FILE_APPEND);
 
         
@@ -26,7 +30,10 @@ class DeviceDataService implements DeviceDataInterface {
     public function ProcessDataGet($device) : DeviceDataModel{
 
         $path = self::RelativePath.$device;
-        
+        $path = strtolower($path);
+
+       
+
         $name = file_get_contents($path."/nome.txt",false);
         $value = file_get_contents($path."/valor.txt", false);
         $time = file_get_contents($path."/hora.txt", false);
